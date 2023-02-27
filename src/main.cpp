e70c021d701ce1072cd7782e185bc7e65ac86576
liyanlong@indemind.cn <liyanlong@indemind.cn>
2023-02-27 12:07:24 +0800

    use glog

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "client/linux/handler/exception_handler.h"
 #include <iostream>
+#include <glog/logging.h>
+
+static const char* EXE_NAME = "breakpad_samples";
+static void FailureHandle(const char* data, int size) {
+    LOG(ERROR) << std::string(data, size);
+}
 
 static bool dumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
                         void* context,
                         bool succeeded)
 {
     std::cout << "Dump path:" << descriptor.path() << std::endl;
+    LOG(INFO) << "Dump path:" << descriptor.path();
     return succeeded;
 }
 void crash()
@@ -15,6 +22,12 @@ void crash()
 }
 int main(int argc, char* argv[])
 {
+    //glog
+    google::InitGoogleLogging(EXE_NAME);
+    google::InstallFailureSignalHandler();
+    google::InstallFailureWriter(FailureHandle);
+    FLAGS_log_dir = "/home/li/log";
+    //breakpad
     google_breakpad::MinidumpDescriptor descriptor("/tmp");
     google_breakpad::ExceptionHandler eh(descriptor, nullptr, dumpCallback, nullptr, true, -1);
     crash();
